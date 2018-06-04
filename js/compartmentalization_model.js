af4a88e11e27aec670ac7086b34913f1ad65173d
Stephen Shank <sshank314@gmail.com>
2018-06-04 10:50:09 -0400

    Simple cherry count.

--- js/compartmentalization_model.js
+++ js/compartmentalization_model.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Row, Col, Grid, Table } from 'react-bootstrap';
 import 'phylotree/phylotree.css';
+import MathJax from 'react-mathjax2';
 
 const d3 = require('d3');
 const _ = require('underscore');
@@ -9,11 +10,21 @@ require("phylotree");
 
 
 class CompartmentalizationModel extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { cherries: null };
+  }
   componentDidMount() {
     this.runCompartmentTest();
   }
   componentDidUpdate() {
     this.runCompartmentTest();
+  }
+  shouldComponentUpdate(nextProps, nextState) {
+    const different_json = nextProps.json != this.props.json,
+      different_cherries = nextState.cherries != this.state.cherries,
+      should_update = different_json || different_cherries;
+    return should_update;
   }
   runCompartmentTest() {
     if(this.props.json) {
@@ -26,7 +37,7 @@ class CompartmentalizationModel extends Component {
           'left-right-spacing': 'fit-to-size',
           'top-bottom-spacing': 'fit-to-size'
         })
-        .size([600, 600]);
+        .size([800, 600]);
       tree(newick)
         .layout();
       const label_to_color = d3.scale.category10()
@@ -44,9 +55,18 @@ class CompartmentalizationModel extends Component {
         tree.modify_selection(node => {
           return regex.test(node.target.name)
         })
-        tree.max_parsimony(true);
       });
-      tree.update();
+      var cherries = 0;
+      tree.traverse_and_compute(function(node){
+        if(node.parent) {
+          const is_unvisited_cherry = _.every(node.parent.children, function(node) {
+            return d3.layout.phylotree.is_leafnode(node) && !node.cherry_visit;
+          });
+          cherries += is_unvisited_cherry;
+        }
+        node.cherry_visit = true;
+      });
+      this.setState({ cherries: cherries });
     }
   }
   render() {
@@ -55,19 +75,23 @@ class CompartmentalizationModel extends Component {
         <svg id="tree_display" width={600} height={600}></svg>          
       </Col>
       <Col xs={4}>
-        <Table striped hover>
-          <thead>
-            <tr>
-              <th>Metric</th>
-              <th>Value</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              
-            </tr>
-          </tbody>
-        </Table>
+        <p>Assortativity index distribution will go here.</p>
+        <MathJax.Context input='tex'>
+          <Table striped hover>
+            <thead>
+              <tr>
+                <th>Metric</th>
+                <th>Value</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td><MathJax.Node>{'C'}</MathJax.Node></td>
+                <td><MathJax.Node>{this.state.cherries}</MathJax.Node></td>
+              </tr>
+            </tbody>
+          </Table>
+        </MathJax.Context>
       </Col>
     </Row>); 
   }
